16078c64a4fff976d7bc329b1694b693fb9e9928
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-01-28 16:40:16 +0100

    added input bootstrap

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ MILESTONE 1
 *Stampare all'interno di una lista, un item per ogni todo.
 *Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 MILESTONE 2
-Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+*Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": 
 cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, 
