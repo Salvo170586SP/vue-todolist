d93476e55c9e033c2f7cbb36652bf19444f3105e
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-01-28 16:37:05 +0100

    added deleteItem

--- js/script.js
+++ js/script.js
@@ -25,18 +25,19 @@ Vue.config.devtools = true;
 const root = new Vue({
     el: '#root',
     data: {
-        
-       tasks: [
-            { text: 'comprare il pane', done: true },
+
+        tasks: [
+            { text: 'comprare il pane', done: false },
             { text: 'studiare js', done: false },
             { text: 'comprare regalo compleanno', done: false },
             { text: 'andare alla posta', done: false }
         ]
     },
 
-    methods:{
-       isChecked(){
-           
-       }
+    methods: {
+        //*ELIMINO UN ELEMENTO SPECIFICO DALL'ARRAY
+        deleteItem(index) {
+            this.tasks.splice(index, 1)
+        }
     }
 })
