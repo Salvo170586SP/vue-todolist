0d7e14da259ecb37aaa41136adfa679731430ec1
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-01-28 15:28:23 +0100

    added v-for li

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@ console.log('Vue ok', Vue);
 Vue.config.devtools = true;
 
 const root = new Vue({
-        el: '#root',
-        data:{
-            
-        }
-    })
+    el: '#root',
+    data: {
+       tasks: [
+            { text: 'comprare il pane', done: false },
+            { text: 'studiare js', done: false },
+            { text: 'comprare regalo compleanno', done: false },
+            { text: 'andare alla posta', done: false }
+        ]
+    }
+
+})
