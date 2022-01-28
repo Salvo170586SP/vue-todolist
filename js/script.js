4278009057f3973b3d9620157e40ecea99bfb676
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-01-28 17:26:11 +0100

    added keyup enter

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ MILESTONE 1
 MILESTONE 2
 *Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 MILESTONE 3
-Predisporre un campo di input testuale e un pulsante "aggiungi": 
-cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, 
-che quindi viene aggiunto alla lista dei todo esistenti.
+*Predisporre un campo di input testuale e un pulsante "aggiungi": 
+*cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, 
+*che quindi viene aggiunto alla lista dei todo esistenti.
 Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente 
